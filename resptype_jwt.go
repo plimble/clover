8f661f792c15bd204df3378cee105e32331c5887
Witoo Harianto <witoo@outlook.com>
2015-03-24 15:01:08 +0700

    update

--- resptype_jwt.go
+++ resptype_jwt.go
@@ -1,6 +1,7 @@
 package clover
 
 import (
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/plimble/unik"
 	"strings"
@@ -19,6 +20,33 @@ func newJWTResponseType(config *AuthConfig) *jwtResponseType {
 		config:        config,
 		tokenRespType: newTokenRespType(config),
 	}
+}
+
+func (rt *jwtResponseType) GetAuthResponse(ar *authorizeRequest, client Client, scopes []string) *Response {
+	at, resp := rt.createToken(client.GetClientID(), client.GetUserID(), scopes)
+	if resp != nil {
+		return resp
+	}
+
+	data := rt.createRespData(at.AccessToken, rt.config.AuthCodeLifetime, scopes, "", ar.state)
+
+	return newRespData(data).setRedirect(ar.redirectURI, ar.responseType, ar.state)
+}
+
+func (rt *jwtResponseType) GetAccessToken(td *TokenData, includeRefresh bool) *Response {
+	at, resp := rt.createToken(td.GrantData.ClientID, td.GrantData.UserID, td.Scope)
+	if resp != nil {
+		return resp
+	}
+
+	refresh, resp := rt.createRefreshToken(at, includeRefresh)
+	if resp != nil {
+		return resp
+	}
+
+	data := rt.createRespData(at.AccessToken, rt.config.AccessLifeTime, at.Scope, refresh, "")
+
+	return newRespData(data)
 }
 
 func (rt *jwtResponseType) createToken(clientID, userID string, scopes []string) (*AccessToken, *Response) {
